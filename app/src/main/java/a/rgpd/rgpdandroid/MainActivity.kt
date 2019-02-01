5953a844ab149f90d00c13203b90a7a34a876cbe
Jodie Boyer <jodieboyer@iMac-de-Jodie.local>
2019-02-01 14:17:31 +0100

    remove: useless code in the app folder

--- app/src/main/java/a/rgpd/rgpdandroid/MainActivity.kt
+++ app/src/main/java/a/rgpd/rgpdandroid/MainActivity.kt
@@ -1,37 +1,12 @@
 package a.rgpd.rgpdandroid
 
-import a.rgpd.rgpd.utils.RGPD
-import a.rgpd.rgpd.utils.RGPDButton
-import a.rgpd.rgpd.utils.RGPDButtonDelegate
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import kotlinx.coroutines.GlobalScope
-import kotlinx.coroutines.async
 
-class MainActivity : AppCompatActivity(), RGPDButtonDelegate {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        RGPDButton.delegate = this
-
-        RGPD.shared.configure("application.test", this)
-    }
-
-    override fun onResume() {
-        super.onResume()
-        RGPD.shared.hasAllAuthorizations {
-            if (!it) {
-                RGPD.shared.showRGPDModally(this@MainActivity)
-                GlobalScope.async {
-
-                }
-            }
-        }
-    }
-
-    override fun didAskForDeletion() {
-        println("Account deleted")
     }
 }
